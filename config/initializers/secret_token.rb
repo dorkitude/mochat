af8b96b2faa876762f7f70edacd2e2fa15fb7a7d
dorkitude <kyle@kylewild.com>
2011-06-25 19:18:36 -0700

    mochat initial commit; new rails proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mochat::Application.config.secret_token = '9962acda43d47333726126b1cb0d9fe943914984691c249db5ede31ff87c425bbc058547849b3df1715ca688cf0ac5c8df5e94eff1f70b10d2ed95d60b3d251e'
