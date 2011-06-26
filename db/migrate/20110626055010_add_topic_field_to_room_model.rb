1f46f13025e8b1b7c388b392deb6b3d68783ca44
dorkitude <kyle@kylewild.com>
2011-06-25 22:52:04 -0700

    add 'topic' field to Room model

---
+++ db/migrate/20110626055010_add_topic_field_to_room_model.rb
@@ -0,0 +1,9 @@
+class AddTopicFieldToRoomModel < ActiveRecord::Migration
+  def self.up
+    add_column :rooms, :topic, :string
+  end
+
+  def self.down
+    remove_column :rooms, :topic
+  end
+end
